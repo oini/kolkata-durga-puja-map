87c5e5b800de46fc8450d3f12762541b1d0e553e
Oindrila Gupta <oindrila@mapbox.com>
2016-07-28 15:12:06 +0530

    Initial map style

---
+++ index.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoib2luaSIsImEiOiJOeGF5QzNZIn0.6OgrVXKq-v2DRpZFU55wDw';
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/oini/cir64nkbx000dcjm67zuhf4gn', //stylesheet location
+    center: [88.3664,22.5606], // starting position
+    zoom: 11 // starting zoom
+});
