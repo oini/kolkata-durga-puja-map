ae6a83287bd543b5e5d42581345a7a4e4275aaba
Oindrila Gupta <oindrila@mapbox.com>
2016-07-28 18:11:36 +0530

    Change map style

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoib2luaSIsImEiOiJOeGF5QzNZIn0.6OgrVXKq-v2DRpZFU55wDw';
 var map = new mapboxgl.Map({
     container: 'map', // container id
-    style: 'mapbox://styles/oini/cir64nkbx000dcjm67zuhf4gn', //stylesheet location
+    style: 'mapbox://styles/oini/cir6b3s450001bvnlgzdbbe6g', //stylesheet location
     center: [88.3664,22.5606], // starting position
     zoom: 11 // starting zoom
 });
